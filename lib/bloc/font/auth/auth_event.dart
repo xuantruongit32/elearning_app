32654f879faf3a0a79e9d30bfebae41749c4978e
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-08 08:17:29 +0700

    feat: auth_event

---
+++ lib/bloc/font/auth/auth_event.dart
@@ -0,0 +1,64 @@
+import 'package:equatable/equatable.dart';
+import 'package:elearning_app/models/user_model.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthStateChanged extends AuthEvent {
+  final UserModel? user;
+
+  const AuthStateChanged(this.user);
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class RegisterRequested extends AuthEvent {
+  final String email;
+  final String password;
+  final String fullName;
+  final UserRole role;
+
+  const RegisterRequested({
+    required this.email,
+    required this.password,
+    required this.fullName,
+    required this.role,
+  });
+
+  @override
+  List<Object?> get props => [email, password, fullName, role];
+}
+
+class LoginRequested extends AuthEvent {
+  final String email;
+  final String password;
+
+  const LoginRequested({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class LogoutRequested extends AuthEvent {
+  final String email;
+
+  const LogoutRequested({required this.email});
+
+  @override
+  List<Object> get props => [email];
+}
+
+class UpdateProfileRequested extends AuthEvent {
+  final String? fullName;
+  final String? photoUrl;
+
+  const UpdateProfileRequested({this.fullName, this.photoUrl});
+
+  @override
+  List<Object?> get props => [fullName, photoUrl];
+}
