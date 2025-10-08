b9ee10e93fda29a099dd728fba9191f90d785f20
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-08 16:07:58 +0700

    style: add onboarding images

---
+++ lib/models/onboarding_item.dart
@@ -0,0 +1,11 @@
+class OnboardingItem {
+  final String image;
+  final String title;
+  final String description;
+
+  OnboardingItem({
+    required this.image,
+    required this.description,
+    required this.title,
+  });
+}
