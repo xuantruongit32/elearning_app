375da6900c2825e1c4c7cc84fbfb2e8179066f2c
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-08 19:53:26 +0700

    fix: onPressed method

--- lib/view/onboarding/widgets/common/custom_button.dart
+++ lib/view/onboarding/widgets/common/custom_button.dart
@@ -26,6 +26,68 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    final theme = Theme.of(context);
+
+    return SizedBox(
+      width: isFullWidth ? double.infinity : null,
+      height: height,
+      child: isOutlined
+          ? OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: backgroundColor ?? theme.primaryColor),
+              ),
+              onPressed: isLoading ? null : onPressed,
+              child: _buildButtonContent(theme),
+            )
+          : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: backgroundColor ?? theme.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+              ),
+              onPressed: isLoading ? null : onPressed,
+              child: _buildButtonContent(theme),
+            ),
+    );
+  }
+
+  Widget _buildButtonContent(ThemeData theme) {
+    if (isLoading) {
+      return SizedBox(
+        height: 20,
+        width: 20,
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+          valueColor: AlwaysStoppedAnimation<Color>(
+            isOutlined ? theme.primaryColor : Colors.white,
+          ),
+        ),
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (icon != null) ...[
+          Icon(
+            icon,
+            color: isOutlined
+                ? (backgroundColor ?? theme.primaryColor)
+                : (textColor ?? Colors.white),
+          ),
+          const SizedBox(height: 8),
+        ],
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            color: isOutlined
+                ? (backgroundColor ?? theme.primaryColor)
+                : (textColor ?? Colors.white),
+          ),
+        ),
+      ],
+    );
   }
 }
