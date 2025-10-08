84136875f2646bc857038ee04ed39adf080a819b
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-08 09:37:17 +0700

    feat: onboarding screen

---
+++ lib/view/onboarding/onboarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( 
+          'Onboarding Screen',
+        ), 
+      ), 
+    ); 
+  }
+}
