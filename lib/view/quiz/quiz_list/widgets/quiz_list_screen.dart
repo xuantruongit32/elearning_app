d76d719491776ed099b7d970b5aa98f758ac760f
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-09 11:42:46 +0700

    feat: add quizz list screen

---
+++ lib/view/quiz/quiz_list/widgets/quiz_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizListScreen extends StatelessWidget {
+  const QuizListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Quiz List Screen')));
+  }
+}
