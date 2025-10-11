eea423d322653cf24c3090672e4b1ac784295db1
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-11 16:01:00 +0700

    feat: add profile options

---
+++ lib/view/profile/widgets/profile_options.dart
@@ -0,0 +1,53 @@
+import 'package:elearning_app/core/utils/app_dialogs.dart';
+import 'package:elearning_app/routes/app_routes.dart';
+import 'package:elearning_app/view/profile/widgets/profile_option_card.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProfileOptions extends StatelessWidget {
+  const ProfileOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ProfileOptionCard(
+          title: 'Edit Profile',
+          subtitle: 'Update your personal information',
+          icon: Icons.edit_outlined,
+          onTap: () {},
+        ),
+        ProfileOptionCard(
+          title: 'Notifications',
+          subtitle: 'Manage your notifications',
+          icon: Icons.notifications_outlined,
+          onTap: () {},
+        ),
+        ProfileOptionCard(
+          title: 'Settings',
+          subtitle: 'Preferences and more',
+          icon: Icons.settings_outlined,
+          onTap: () {},
+        ),
+        ProfileOptionCard(
+          title: 'Help & Support',
+          subtitle: 'Get help or contact support',
+          icon: Icons.help_outlined,
+          onTap: () {},
+        ),
+        ProfileOptionCard(
+          title: 'Logout',
+          subtitle: 'Sign out of your account',
+          icon: Icons.logout,
+          onTap: () async {
+            final confirm = await AppDialogs.showLogoutDialog();
+            if (confirm == true) {
+              Get.offAllNamed(AppRoutes.login);
+            }
+          },
+          isDestructive: true,
+        ),
+      ],
+    );
+  }
+}
