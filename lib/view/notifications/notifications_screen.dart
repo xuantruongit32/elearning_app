06ed65c56ad2ab9a6f354346850554b99b43a803
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-14 12:31:03 +0700

    feat: add notifications screen

--- lib/view/notifications/notifications_screen.dart
+++ lib/view/notifications/notifications_screen.dart
@@ -1,10 +1,52 @@
+import 'package:elearning_app/core/theme/app_colors.dart';
+import 'package:elearning_app/view/notifications/widgets/notification_settings_section.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: AppColors.lightBackground,
+      appBar: AppBar(
+        title: const Text(
+          'Notifications',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: AppColors.primary,
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  NotificationSettingsSection(),
+                  const SizedBox(height: 24),
+                  Text(
+                    'Recent Notifications',
+                    style: theme.textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
