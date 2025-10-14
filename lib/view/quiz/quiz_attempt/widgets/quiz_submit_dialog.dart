4c6f69e92f3db9fc8d21753cfbfbf43a0e657003
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-14 10:18:21 +0700

    feat: quiz submit dialog

--- lib/view/quiz/quiz_attempt/widgets/quiz_submit_dialog.dart
+++ lib/view/quiz/quiz_attempt/widgets/quiz_submit_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:elearning_app/models/quiz.dart';
 import 'package:elearning_app/models/quiz_attempt.dart';
+import 'package:elearning_app/view/quiz/quiz_result/quiz_result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class QuizSubmitDialog extends StatelessWidget {
   final QuizAttempt attempt;
@@ -13,6 +15,22 @@ class QuizSubmitDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AlertDialog(
+      title: const Text("Submit Quiz"),
+      content: const Text("Are you sure you want to submit your answers?"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        FilledButton(
+          onPressed: () {
+            Navigator.pop(context);
+            Get.off(() => QuizResultScreen(attempt: attempt, quiz: quiz));
+          },
+          child: const Text('Submit'),
+        ),
+      ],
+    );
   }
 }
