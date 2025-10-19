b2518b233d23fa457aa27a78cc2273d9e475b0e1
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-19 16:50:52 +0700

    feat: add course respository

---
+++ lib/respositories/course_respository.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:elearning_app/models/course.dart';
+
+class CourseRepository {
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<void> createCourse(Course course) async {
+    try {
+      final courseData = course.toJson();
+      final lessonsData = course.lessons
+          .map((lesson) => lesson.toJson())
+          .toList();
+
+      await _firestore.collection('courses').doc(course.id).set({
+        ...courseData,
+        'lessons': lessonsData,
+      });
+    } catch (e) {
+      throw Exception('Failed to create course: $e');
+    }
+  }
+}
