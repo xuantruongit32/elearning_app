7710758fc98b4bf4c56e6723d3a73f1a44eca51c
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-19 16:51:26 +0700

    feat: add uploadVideo func in cloudinary service

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -3,14 +3,40 @@ import 'package:cloudinary_public/cloudinary_public.dart';
 class CloudinaryService {
   final cloudinary = CloudinaryPublic('dygc3825n', 'tt-elearning-app');
 
-  Future<String> uploadImage(String imagePath) async {
+  Future<String> uploadImage(String imagePath, String folder) async {
     try {
       final response = await cloudinary.uploadFile(
-        CloudinaryFile.fromFile(imagePath, folder: 'profile_pictures'),
+        CloudinaryFile.fromFile(imagePath, folder: folder),
       );
       return response.secureUrl;
     } catch (e) {
       throw Exception('Failed to upload image: $e');
+    }
+  }
+
+  Future<String> uploadVideo(String videoPath) async {
+    try {
+      final response = await cloudinary.uploadFile(
+        CloudinaryFile.fromFile(
+          videoPath,
+          folder: 'course_videos',
+          resourceType: CloudinaryResourceType.Video,
+        ),
+      );
+      return response.secureUrl;
+    } catch (e) {
+      throw Exception('Failed to upload video: $e');
+    }
+  }
+
+  Future<String> uploadFile(String filePath) async {
+    try {
+      final response = await cloudinary.uploadFile(
+        CloudinaryFile.fromFile(filePath, folder: 'course_resources'),
+      );
+      return response.secureUrl;
+    } catch (e) {
+      throw Exception('Failed to upload file: $e');
     }
   }
 }
