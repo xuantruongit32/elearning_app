11be2b62cc0f643c7808da485f454faf4a569f60
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-19 09:18:24 +0700

    feat: add blocbuilder for home appbar

--- lib/view/home/widgets/home_app_bar.dart
+++ lib/view/home/widgets/home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:elearning_app/core/theme/app_colors.dart';
 import 'package:elearning_app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 class HomeAppBar extends StatelessWidget {
@@ -10,47 +11,54 @@ class HomeAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return SliverAppBar(
-      expandedHeight: 180,
-      floating: false,
-      pinned: true,
-      backgroundColor: AppColors.primary,
-      actions: [
-        IconButton(
-          onPressed: () => Get.toNamed(AppRoutes.analytics),
-          icon: const Icon(Icons.analytics, color: Colors.white),
-        ),
-      ],
-      flexibleSpace: FlexibleSpaceBar(
-        titlePadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Welcome back,',
-              style: theme.textTheme.bodySmall?.copyWith(
-                color: AppColors.accent.withValues(alpha: 0.7),
-              ),
-            ),
-            Text(
-              'Hoang Truong,',
-              style: theme.textTheme.titleLarge?.copyWith(
-                color: AppColors.accent.withValues(alpha: 0.7),
-              ),
+    return BlocBuilder(
+      builder: (context, state) {
+        return SliverAppBar(
+          expandedHeight: 180,
+          floating: false,
+          pinned: true,
+          backgroundColor: AppColors.primary,
+          actions: [
+            IconButton(
+              onPressed: () => Get.toNamed(AppRoutes.analytics),
+              icon: const Icon(Icons.analytics, color: Colors.white),
             ),
           ],
-        ),
-        background: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [AppColors.primary, AppColors.primaryLight],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 16,
+            ),
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome back,',
+                  style: theme.textTheme.bodySmall?.copyWith(
+                    color: AppColors.accent.withValues(alpha: 0.7),
+                  ),
+                ),
+                Text(
+                  'Hoang Truong,',
+                  style: theme.textTheme.titleLarge?.copyWith(
+                    color: AppColors.accent.withValues(alpha: 0.7),
+                  ),
+                ),
+              ],
+            ),
+            background: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [AppColors.primary, AppColors.primaryLight],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
