5a2f245e288e56c948ee8a2fbc75ada15b65f30d
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-21 14:33:23 +0700

    feat: add getInstructorsByIds in instructor_res

--- lib/respositories/instructor_respositories.dart
+++ lib/respositories/instructor_respositories.dart
@@ -16,5 +16,20 @@ class InstructorRepository {
     }
   }
 
-  
+  Future<Map<String, UserModel>> getInstructorsByIds(
+    List<String> instructorIds,
+  ) async {
+    try {
+      if (instructorIds.isEmpty) return {};
+
+      final docs = await _firestore
+          .collection('users')
+          .where(FieldPath.documentId, whereIn: instructorIds)
+          .get();
+
+      return {for (var doc in docs.docs) doc.id: UserModel.fromFirestore(doc)};
+    } catch (e) {
+      throw Exception('Failed to get instructors: $e');
+    }
+  }
 }
