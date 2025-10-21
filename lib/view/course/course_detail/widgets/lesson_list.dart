dd146817ef962425fdf82b1d52778c9fdd407d2f
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-21 16:47:53 +0700

    feat: fetch lesson from firebase

--- lib/view/course/course_detail/widgets/lesson_list.dart
+++ lib/view/course/course_detail/widgets/lesson_list.dart
@@ -1,11 +1,13 @@
 import 'package:elearning_app/core/theme/app_colors.dart';
+import 'package:elearning_app/models/course.dart';
+import 'package:elearning_app/respositories/course_respository.dart';
 import 'package:elearning_app/routes/app_routes.dart';
 import 'package:elearning_app/services/dummy_data_service.dart';
 import 'package:elearning_app/view/course/course_detail/widgets/lesson_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LessonList extends StatelessWidget {
+class LessonList extends StatefulWidget {
   final String courseId;
   final bool isUnlocked;
   final VoidCallback? onLessonComplete;
@@ -17,30 +19,75 @@ class LessonList extends StatelessWidget {
   });
 
   @override
+  State<LessonList> createState() => _LessonListState();
+}
+
+class _LessonListState extends State<LessonList> {
+  final CourseRepository _courseRepository = CourseRepository();
+  Course? _course;
+  bool _isLoading = true;
+  Set<String> _completedLessons = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCourse();
+  }
+
+  Future<void> _loadCourse() async {
+    try {
+      final course = await _courseRepository.getCourseDetail(widget.courseId);
+      final completedLessons = await _courseRepository.getCompletedLessons(
+        widget.courseId,
+      );
+      setState(() {
+        _course = course;
+        _completedLessons = completedLessons;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      Get.snackbar(
+        'Error',
+        'Failed to load course lessons',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final course = DummyDataService.getCourseById(courseId);
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (_course == null) {
+      return const Center(child: Text('No Lessons Available'));
+    }
     return ListView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemCount: course.lessons.length,
+      itemCount: _course!.lessons.length,
       itemBuilder: (context, index) {
-        final lesson = course.lessons[index];
+        final lesson = _course!.lessons[index];
         final isLocked =
             !lesson.isPreview &&
             (index > 0 &&
                 !DummyDataService.isLessonCompleted(
-                  course.id,
-                  course.lessons[index - 1].id,
+                  _course!.id,
+                  _course!.lessons[index - 1].id,
                 ));
         return LessonTile(
           title: lesson.title,
           duration: '${lesson.duration} mins',
-          isCompleted: DummyDataService.isLessonCompleted(course.id, lesson.id),
+          isCompleted: DummyDataService.isLessonCompleted(_course!.id, lesson.id),
           isLocked: isLocked,
 
-          isUnlocked: isUnlocked,
+          isUnlocked: widget.isUnlocked,
           onTap: () async {
-            if (course.isPremium && !isUnlocked) {
+            if (_course!.isPremium && !widget.isUnlocked) {
               Get.snackbar(
                 'Premium Course',
                 'Please purchase this course to access all lessons',
@@ -59,10 +106,10 @@ class LessonList extends StatelessWidget {
             } else {
               final result = await Get.toNamed(
                 AppRoutes.lesson.replaceAll(':id', lesson.id),
-                parameters: {'courseId' : courseId},
+                parameters: {'courseId': widget.courseId},
               );
-              if(result == true){
-                onLessonComplete?.call();
+              if (result == true) {
+                widget.onLessonComplete?.call();
               }
             }
           },
@@ -71,4 +118,3 @@ class LessonList extends StatelessWidget {
     );
   }
 }
- 
