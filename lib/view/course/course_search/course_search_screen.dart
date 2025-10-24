278a38974bfa42e59631bf3f2687d68439a35ae8
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-24 10:01:10 +0700

    feat: add _onSearchChanged in course search screen

--- lib/view/course/course_search/course_search_screen.dart
+++ lib/view/course/course_search/course_search_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:elearning_app/bloc/filtered_course/filtered_course_bloc.dart';
+import 'package:elearning_app/bloc/filtered_course/filtered_course_event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CourseSearchScreen extends StatefulWidget {
   const CourseSearchScreen({super.key});
@@ -13,6 +16,31 @@ class _CourseSearchScreenState extends State<CourseSearchScreen> {
   final _searchController = TextEditingController();
   final _debounce = Debouncer(milliseconds: 500);
   final _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    _searchController.addListener(_onSearchChanged);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    _debounce.run(() {
+      final query = _searchController.text.trim();
+      if (query.isNotEmpty) {
+        context.read<FilteredCourseBloc>().add(SearchCourses(query));
+      } else {
+        context.read<FilteredCourseBloc>().add(ClearFilteredCourses());
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
