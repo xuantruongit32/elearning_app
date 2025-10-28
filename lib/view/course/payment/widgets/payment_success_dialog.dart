6035310bd59487f4d91c696ae3c67fd28e0a9788
xuantruongit32 <xuantruongit32@gmail.com>
2025-10-28 11:01:03 +0700

    feat: change en - vn in payment success dialog

--- lib/view/course/payment/widgets/payment_success_dialog.dart
+++ lib/view/course/payment/widgets/payment_success_dialog.dart
@@ -2,7 +2,6 @@ import 'package:elearning_app/core/theme/app_colors.dart';
 import 'package:elearning_app/view/onboarding/widgets/common/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 class PaymentSuccessDialog extends StatelessWidget {
   const PaymentSuccessDialog({super.key});
@@ -19,14 +18,14 @@ class PaymentSuccessDialog extends StatelessWidget {
             Icon(Icons.check_circle, color: AppColors.primary, size: 64),
             const SizedBox(height: 16),
             Text(
-              'Payment Successful!',
+              'Thanh toán thành công!',
               style: Get.textTheme.titleLarge?.copyWith(
                 fontWeight: FontWeight.bold,
               ),
             ),
             const SizedBox(height: 8),
             Text(
-              'You can now access the course content',
+              'Bạn có thể truy cập khoá học ngay.',
               style: Get.textTheme.bodyMedium,
               textAlign: TextAlign.center,
             ),
@@ -34,13 +33,11 @@ class PaymentSuccessDialog extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               height: 56,
-
               child: CustomButton(
-                text: 'Start Learning',
+                text: 'Bắt đầu học',
                 onPressed: () {
                   Get.back();
                   Get.back();
-                  // navigate to course content
                 },
               ),
             ),
