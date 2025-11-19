4c75483a978d934cc17c2440015810fa094fc0f4
xuantruongit32 <xuantruongit32@gmail.com>
2025-11-19 19:03:00 +0700

    feat: add fromSnapshot func in category firestore

--- lib/models/category_firestore.dart
+++ lib/models/category_firestore.dart
@@ -4,18 +4,30 @@ class CategoryFirestoreModel {
   final String id;
   final String name;
   final String icon;
+
   CategoryFirestoreModel({
     required this.id,
     required this.name,
     required this.icon,
   });
 
+  // Hàm cũ của bạn (giữ lại để tương thích ngược nếu cần)
   factory CategoryFirestoreModel.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return CategoryFirestoreModel(
       id: doc.id,
       name: data['name'] ?? '',
       icon: (data['icon'] ?? ''),
+    );
+  }
+
+  // --- MỚI THÊM: Hàm fromSnapshot ---
+  factory CategoryFirestoreModel.fromSnapshot(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return CategoryFirestoreModel(
+      id: doc.id,
+      name: data['name'] ?? '',
+      icon: data['icon'] ?? '',
     );
   }
 
