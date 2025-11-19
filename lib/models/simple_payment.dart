15905655e31d22c869da2840533685f6ccb852b6
xuantruongit32 <xuantruongit32@gmail.com>
2025-11-19 13:35:22 +0700

    feat: new simple payment models

--- lib/models/simple_payment.dart
+++ lib/models/simple_payment.dart
@@ -5,7 +5,18 @@ class SimplePayment {
   final double amount;
   final Timestamp date;
 
-  SimplePayment({required this.id, required this.amount, required this.date});
+  final String studentId;
+  final String teacherId;
+  final String courseId;
+
+  SimplePayment({
+    required this.id,
+    required this.amount,
+    required this.date,
+    required this.studentId,
+    required this.teacherId,
+    required this.courseId,
+  });
 
   factory SimplePayment.fromSnapshot(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
@@ -13,6 +24,19 @@ class SimplePayment {
       id: doc.id,
       amount: (data['amount'] as num).toDouble(),
       date: data['date'] as Timestamp,
+      studentId: data['studentId'] as String,
+      teacherId: data['teacherId'] as String,
+      courseId: data['courseId'] as String,
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'amount': amount,
+      'date': date,
+      'studentId': studentId,
+      'teacherId': teacherId,
+      'courseId': courseId,
+    };
   }
 }
